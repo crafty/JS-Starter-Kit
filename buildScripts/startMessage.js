eabed31d522021a16259d2f6e6bdb899201cf575
Dan Craycraft <dan.craycraft@goodwill.gits>
2019-02-21 15:51:33 -0500

    testing travisCI

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk');
+import chalk from 'chalk'; //es6 standard, babel gives us these features to node can understand it
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
